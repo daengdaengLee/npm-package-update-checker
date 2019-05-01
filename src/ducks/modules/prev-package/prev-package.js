edc0e6d9315219ea5414ca554cc17c47247165f6
daengdaengLee <gunho1020@gmail.com>
2019-05-01 17:36:44 +0900

    prev-package/CHANGE_PACKAGE_STRING 액션 구현

--- src/ducks/modules/prev-package/prev-package.js
+++ src/ducks/modules/prev-package/prev-package.js
@@ -1,6 +1,13 @@
 // Actions
 
+export const CHANGE_PACKAGE_STRING = "prev-package/CHANGE_PACKAGE_STRING";
+
 // Action Creators
+
+export const changePackageString = string => ({
+  type: CHANGE_PACKAGE_STRING,
+  string
+});
 
 // Init State
 
@@ -13,9 +20,18 @@ const initState = {
 
 export default function prevPackageReducer(state = initState, action = {}) {
   switch (action.type) {
+    case CHANGE_PACKAGE_STRING:
+      return applyChangePackageString(state, action);
     default:
       return state;
   }
 }
 
 // Reducer Functions
+
+function applyChangePackageString(state, { string }) {
+  return {
+    ...state,
+    string
+  };
+}
