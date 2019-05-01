a69df96407dc6fdfd2296924d1cd41240467cad4
daengdaengLee <gunho1020@gmail.com>
2019-05-01 22:21:01 +0900

    getNpmPackageVersion 함수 구현

--- src/requests/get-npm-package-version.js
+++ src/requests/get-npm-package-version.js
@@ -1,3 +1,10 @@
-export const getNpmPackageVersion = name => {
-  return { success: true, version: "test" };
+export const getNpmPackageVersion = async name => {
+  try {
+    const response = await fetch(`npm-package/${name}`);
+    const { latestVersion } = response.json();
+
+    return { success: true, version: `^${latestVersion}` };
+  } catch {
+    return { success: false, version: "" };
+  }
 };
