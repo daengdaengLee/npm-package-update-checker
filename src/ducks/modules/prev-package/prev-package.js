3843c761b184f350c5d3109d272c0b3ccd18b147
daengdaengLee <gunho1020@gmail.com>
2019-05-01 17:27:25 +0900

    prev-package 리듀서 초기화

---
+++ src/ducks/modules/prev-package/prev-package.js
@@ -0,0 +1,21 @@
+// Actions
+
+// Action Creators
+
+// Init State
+
+const initState = {
+  string: "",
+  object: null
+};
+
+// Reducer
+
+export default function prevPackageReducer(state = initState, action = {}) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+// Reducer Functions
