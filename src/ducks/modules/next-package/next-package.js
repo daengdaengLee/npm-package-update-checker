c57523bc5a8b0368cbdacae09166cbec2940ca9c
daengdaengLee <gunho1020@gmail.com>
2019-05-01 19:06:46 +0900

    next-package/CHANGE_DEPENDENCIES 액션 구현

--- src/ducks/modules/next-package/next-package.js
+++ src/ducks/modules/next-package/next-package.js
@@ -1,12 +1,19 @@
+import * as F from "fxjs2";
+
 // Actions
 
 export const CHANGE_PACKAGE_OBJECT = "next-package/CHANGE_PACKAGE_OBJECT";
+export const CHANGE_DEPENDENCIES = "next-package/CHANGE_DEPENDENCIES";
 
 // Action Creators
 
 export const changePackageObject = packageObject => ({
   type: CHANGE_PACKAGE_OBJECT,
   packageObject
+});
+export const changeDependencies = dependencies => ({
+  type: CHANGE_DEPENDENCIES,
+  dependencies
 });
 
 // Init State
@@ -24,6 +31,8 @@ export default function nextPackageReducer(state = initState, action = {}) {
   switch (action.type) {
     case CHANGE_PACKAGE_OBJECT:
       return applyChangePackageObject(state, action);
+    case CHANGE_DEPENDENCIES:
+      return applyChangeDependencies(state, action);
     default:
       return state;
   }
@@ -36,4 +45,18 @@ function applyChangePackageObject(
   { packageObject: { dependencies, devDependencies, ...packageObject } }
 ) {
   return { ...state, packageObject };
+}
+
+function applyChangeDependencies(state, { dependencies }) {
+  return {
+    ...state,
+    dependencies: F.reduce(
+      (dependencies, dependency) => {
+        dependencies.set(dependency.name, dependency);
+        return dependencies;
+      },
+      new Map(),
+      dependencies
+    )
+  };
 }
