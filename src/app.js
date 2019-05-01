e31e64d98ca2298d718fdc1825f0310b602486d1
Kunho Lee <gunho1020@gmail.com>
2019-05-01 23:54:45 +0900

    ENH #15(Finish): 기본 UI 구성 (#16)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
+import { connect } from "react-redux";
+
 import { withErrorBoundary } from "./hocs";
+
+import { requestCheckUpdates } from "./ducks/modules/next-package/next-package";
 
 import AppPresenter from "./app.presenter";
 
-const App = withErrorBoundary(AppPresenter);
+const mapStateToProps = ({
+  nextPackage: { packageObject, dependencies, devDependencies }
+}) => ({
+  nextPackageObject: packageObject,
+  nextDependencies: dependencies,
+  nextDevDependencies: devDependencies
+});
+
+const mapDispatchToProps = dispatch => ({
+  onClickCheck: packageString => dispatch(requestCheckUpdates(packageString))
+});
+
+const App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withErrorBoundary(AppPresenter));
 
 export default App;
--- src/app.presenter.js
+++ src/app.presenter.js
@@ -1,5 +1,107 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import * as F from "fxjs2";
+import { L } from "fxjs2";
 
-const App = () => <div>hello world</div>;
+const Container = styled.div`
+  width: 100%;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 24rem;
+
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+`;
+
+const App = ({
+  nextPackageObject,
+  nextDependencies,
+  nextDevDependencies,
+  onClickCheck
+}) => {
+  const [prevPackageJsonString, setPrevPackageJsonString] = useState("");
+  const nextPackageJsonString = useMemo(() => {
+    if (!nextPackageObject || typeof nextPackageObject !== "object") return "";
+
+    const dependencies = F.go(
+      nextDependencies,
+      L.map(([name, { prevVersion, nextVersion }]) => ({
+        name,
+        version: nextVersion || prevVersion
+      })),
+      iter =>
+        F.reduce(
+          (dependencies, dependency) => {
+            dependencies[dependency.name] = dependency.version;
+            return dependencies;
+          },
+          {},
+          iter
+        )
+    );
+    const devDependencies = F.go(
+      nextDevDependencies,
+      L.map(([name, { prevVersion, nextVersion }]) => ({
+        name,
+        version: nextVersion || prevVersion
+      })),
+      iter =>
+        F.reduce(
+          (devDependencies, devDependency) => {
+            devDependencies[devDependency.name] = devDependency.version;
+            return devDependencies;
+          },
+          {},
+          iter
+        )
+    );
+
+    const packageObject = {
+      ...nextPackageObject,
+      dependencies,
+      devDependencies
+    };
+
+    return JSON.stringify(packageObject, null, 2);
+  }, [nextPackageObject, nextDependencies, nextDevDependencies]);
+
+  return (
+    <Container>
+      <div>prev package.json</div>
+      <TextArea
+        value={prevPackageJsonString}
+        onChange={({ target: { value } }) => setPrevPackageJsonString(value)}
+      />
+      <div>
+        <button
+          type="button"
+          onClick={() => onClickCheck(prevPackageJsonString)}
+        >
+          check
+        </button>
+      </div>
+      <div>next package.json</div>
+      <TextArea value={nextPackageJsonString} readOnly />
+    </Container>
+  );
+};
+
+App.defaultProps = {
+  nextPackageObject: {},
+  nextDependencies: new Map(),
+  nextDevDependencies: new Map(),
+  onClickCheck: prevPackageJsonString => {}
+};
+
+App.propTypes = {
+  nextPackageObject: PropTypes.object,
+  nextDependencies: PropTypes.instanceOf(Map),
+  nextDevDependencies: PropTypes.instanceOf(Map),
+  onClickCheck: PropTypes.func
+};
 
 export default App;
