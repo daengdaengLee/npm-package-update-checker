ec0efc85d2fab04ec9c428af48fd1b7c280f7bef
daengdaengLee <gunho1020@gmail.com>
2019-05-03 12:46:36 +0900

    prev-package 컴포넌트 구현

--- src/components/organisms/prev-package/prev-package.js
+++ src/components/organisms/prev-package/prev-package.js
@@ -1,7 +1,18 @@
+import { connect } from "react-redux";
+
 import { withErrorBoundary } from "../../../hocs";
+
+import { requestCheckUpdates } from "../../../ducks/modules/next-package/next-package";
 
 import PrevPackagePresenter from "./prev-package.presenter";
 
-const PrevPackage = withErrorBoundary(PrevPackagePresenter);
+const mapDispatchToProps = dispatch => ({
+  onClickCheck: packageString => dispatch(requestCheckUpdates(packageString))
+});
+
+const PrevPackage = connect(
+  null,
+  mapDispatchToProps
+)(withErrorBoundary(PrevPackagePresenter));
 
 export default PrevPackage;
--- src/components/organisms/prev-package/prev-package.presenter.js
+++ src/components/organisms/prev-package/prev-package.presenter.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-const PrevPackage = () => <div>prev package</div>;
+const Container = styled.div`
+  width: 100%:
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 24rem;
+
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+`;
+
+const PrevPackage = ({ onClickCheck }) => {
+  const [prevPackageString, setPrevPackageString] = useState("");
+
+  return (
+    <Container>
+      <div>prev package.json</div>
+      <div>
+        <button type="button" onClick={() => onClickCheck(prevPackageString)}>
+          check
+        </button>
+      </div>
+      <TextArea
+        value={prevPackageString}
+        onChange={({ target: { value } }) => setPrevPackageString(value)}
+      />
+    </Container>
+  );
+};
+
+PrevPackage.defaultProps = {
+  onClickCheck: prevPackageString => {}
+};
+
+PrevPackage.propTypes = {
+  onClickCheck: PropTypes.func
+};
 
 export default PrevPackage;
