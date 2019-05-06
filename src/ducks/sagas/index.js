c0d22a31cec16dc8130311a39970b50d941f8776
daengdaengLee <gunho1020@gmail.com>
2019-05-07 00:48:42 +0900

    root 사가 작성

---
+++ src/ducks/sagas/index.js
@@ -0,0 +1,7 @@
+import { all } from 'redux-saga/effects';
+
+import nextPackageSaga from './next-package/next-package';
+
+export default function* rootSaga() {
+  yield all([nextPackageSaga()]);
+}
