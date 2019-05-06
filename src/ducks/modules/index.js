e25c8fcc28e482a90273a0cd883e66a3a3c6d4e1
daengdaengLee <gunho1020@gmail.com>
2019-05-07 00:44:13 +0900

    root 리듀서 생성

---
+++ src/ducks/modules/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import nextPackage from './next-package/next-package';
+
+const rootReducer = combineReducers({ nextPackage });
+
+export default rootReducer;
